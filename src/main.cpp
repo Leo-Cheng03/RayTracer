de24037c6cf52a2437b27b87c1ebdddd15c69c2c
Leo Cheng <3089288874leo@gmail.com>
2024-03-23 22:27:04 +0800

    add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Parse configuration file.
+    // Initialize renderer.
+    // Parse scene description file.
+    // Render scene.
+    // Clean up.
+    return 0;
+}
