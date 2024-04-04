1dc357ad868c0b81690b594e70fc84dc3e70ef2c
Zheng Xin Le <zhengxl22@mails.tsinghua.edu.cn>
2024-04-04 23:01:50 +0800

    modify Russian roulette to avoid unnecessary calculation

--- src/integrator.cpp
+++ src/integrator.cpp
@@ -14,32 +14,42 @@ Vector3f Integrator::SampleL(const SceneParser& scene, Ray ray) {
     int depth = 0;
 
     while (depth < 10) {
+        // Russian roulette
+        float survivalRate = 1.0f;
+        if (depth > 3) {
+            survivalRate = 0.8f;
+            if (!russianRoulette(survivalRate)) {
+                break;
+            }
+        }
+
         Vector3f color = Vector3f::ZERO;
 
         // Check if the ray intersects with any object in the scene
-        // If it does, get the surface normal and material properties
-            // Direct lighting
-            // Sample a light based on power heuristic
-            // Create a new ray from the intersection point to the light source
-            // Check if the new ray intersects with any object in the scene
-            // If it doesn't, calculate the direct lighting
-            
-            // Indirect lighting
-            // If the material is a light source
-                // Return black
-            // Otherwise, calculate the direct lighting
-            // Create new rays based on the material properties
-        // If it doesn't, account for the background color
+        if (primitives->intersect(ray, hit, 0.001f)) {
+            // If it does, get the surface normal and material properties
+                // Direct lighting
+                // Sample a light based on power heuristic
+                // Create a new ray from the intersection point to the light source
+                // Check if the new ray intersects with any object in the scene
+                // If it doesn't, calculate the direct lighting
+                
+                // Indirect lighting
+                // If the material is a light source
+                    // Return black
+                // Otherwise, calculate the direct lighting
+                // Create new rays based on the material properties
+        }
+        else {
+            // If it doesn't, account for the background color
+            color = scene.getBackgroundColor();
+            finalColor += color;
+            break;
+        }
 
         // Russian roulette
-        if (depth > 3) {
-            if (!russianRoulette(0.8f)) {
-                break;
-            }
-            else {
-                color /= 0.8f;
-            }
-        }
+        color /= survivalRate;
+        finalColor += color;
     }
 
     return Vector3f(0, 0, 0);
