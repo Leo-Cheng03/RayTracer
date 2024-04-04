64cfa613258617eac4121e0d98d3543fffb8dc54
Zheng Xin Le <zhengxl22@mails.tsinghua.edu.cn>
2024-04-04 19:42:25 +0800

    add BSDF class

---
+++ include/bsdf.hpp
@@ -0,0 +1,21 @@
+#ifndef BSDF_HPP
+#define BSDF_HPP
+
+#include <vecmath.h>
+
+struct BSDFSample {
+
+};
+
+class BSDF {
+public:
+    BSDF() {}
+    ~BSDF() = default;
+
+    virtual Vector3f f(Vector3f wo, Vector3f wi) = 0;
+    virtual BSDFSample Sample_f(Vector3f wo) = 0;
+
+protected:
+};
+
+#endif
