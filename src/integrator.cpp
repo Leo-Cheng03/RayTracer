d41918379f2cde08607b59bdf7956ba11f732f55
Zheng Xin Le <zhengxl22@mails.tsinghua.edu.cn>
2024-04-04 22:56:06 +0800

    add Russian roulette to the rendering loop

--- src/integrator.cpp
+++ src/integrator.cpp
@@ -3,22 +3,44 @@
 #include "hit.hpp"
 #include "light.hpp"
 
+float russianRoulette(float p) {
+    return (float)rand() / RAND_MAX < p;
+}
+
 Vector3f Integrator::SampleL(const SceneParser& scene, Ray ray) {
+    Group* primitives = scene.getGroup();
     Hit hit;
-    Group* objects = scene.getGroup();
-    if (objects->intersect(ray, hit, 0)) {
+    Vector3f finalColor = Vector3f::ZERO;
+    int depth = 0;
+
+    while (depth < 10) {
         Vector3f color = Vector3f::ZERO;
-        for (int li = 0; li < scene.getNumLights(); li++) {
-            Light* light = scene.getLight(li);
-            Vector3f L, lightColor;
-            light->getIllumination(ray.pointAtParameter(hit.getT()), L, lightColor);
-            color += hit.getMaterial()->Shade(ray, hit, L, lightColor);
+
+        // Check if the ray intersects with any object in the scene
+        // If it does, get the surface normal and material properties
+            // Direct lighting
+            // Sample a light based on power heuristic
+            // Create a new ray from the intersection point to the light source
+            // Check if the new ray intersects with any object in the scene
+            // If it doesn't, calculate the direct lighting
+            
+            // Indirect lighting
+            // If the material is a light source
+                // Return black
+            // Otherwise, calculate the direct lighting
+            // Create new rays based on the material properties
+        // If it doesn't, account for the background color
+
+        // Russian roulette
+        if (depth > 3) {
+            if (!russianRoulette(0.8f)) {
+                break;
+            }
+            else {
+                color /= 0.8f;
+            }
         }
-        return color;
     }
-    else {
-        return scene.getBackgroundColor();
-    }
-    
+
     return Vector3f(0, 0, 0);
 }
